0692c3fa882513ed7b00046fe780864a28f55d79
Stefan Kairinos <stefan.kairinos@ocado.com>
2023-03-10 11:41:12 +0000

    fix: test pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// test
 test('renders paragraph', () => {
   render(<App />);
   const pElement = screen.getByText(/component library/i);
