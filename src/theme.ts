fc86953f74449959ab1ceb425a673bd08aa1b743
SKairinos <stefan.kairinos@ocado.com>
2023-03-16 15:00:53 +0000

    fix: theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,35 +1,54 @@
+import { PaletteColor } from '@mui/material';
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import { common, red, blue } from '@mui/material/colors';
+import { red } from '@mui/material/colors';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    tertiary: PaletteColor;
+  }
+  interface PaletteOptions {
+    tertiary: PaletteColor;
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    tertiary: true;
+  }
+}
+
+const { palette } = createTheme();
 
 const theme = responsiveFontSizes(createTheme({
   palette: {
     primary: {
-      main: common.black
+      main: '#e0004d'
     },
     secondary: {
-      main: blue.A400
+      main: '#00a3e0'
     },
+    tertiary: palette.augmentColor({
+      color: {
+        main: '#ffd23b'
+      }
+    }),
     error: {
-      main: red.A400
+      main: red.A700
     }
   },
   components: {
     MuiButton: {
-      defaultProps: {
-        disableRipple: true
-      },
       styleOverrides: {
         root: {
-          fontSize: '0.8rem',
-          border: '1px solid black'
+          whiteSpace: 'nowrap',
+          minWidth: 'max-content'
         }
       }
     },
     MuiTextField: {
       styleOverrides: {
         root: {
-          width: '100%',
-          height: '100%'
+          width: '100%'
         }
       }
     }
