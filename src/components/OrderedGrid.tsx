80fe50c525895962ef6134b05cb2b7de43ac4425
SKairinos <stefan.kairinos@ocado.com>
2023-04-13 17:54:56 +0100

    feat: add OrderedGridProps component

---
+++ src/components/OrderedGrid.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Unstable_Grid2 as Grid,
+  Grid2Props
+} from '@mui/material';
+
+interface ItemProps extends Omit<Grid2Props, (
+  'key' | 'order' |
+  'xs' | 'sm' | 'md' | 'lg' | 'xl' |
+  'xsOffset' | 'smOffset' | 'mdOffset' | 'lgOffset' | 'xlOffset'
+)> { }
+
+interface GlobalItemProps extends ItemProps {
+  xs: number
+  sm: number
+  md: number
+  lg: number
+  xl: number
+}
+
+export interface OrderedGridProps {
+  rows: Array<Array<{
+    element: React.ReactElement,
+    itemProps?: ItemProps
+  }>>
+  containerProps?: Omit<Grid2Props, 'container'>
+  globalItemProps: GlobalItemProps
+}
+
+const OrderedGrid: React.FC<OrderedGridProps> = ({
+  rows, containerProps = {}, globalItemProps
+}) => {
+  const columns = Number(containerProps.columns ?? 12);
+
+  const getItemsPerRow = (size: number): number => Math.floor(columns / size);
+
+  const getOrder = (rowIndex: number, itemIndex: number, size: number): number => (
+    (Math.floor(itemIndex / getItemsPerRow(size)) * rows.length) + rowIndex
+  );
+
+  const getOffset = (itemIndex: number, size: number): number => {
+    const itemsOnLastRow = rows[0].length % getItemsPerRow(size);
+    return itemsOnLastRow !== 0 && itemIndex === rows[0].length - 1
+      ? (columns - (itemsOnLastRow * size)) / 2
+      : 0;
+  };
+
+  return (
+    <Grid container {...containerProps}>
+      {rows.map((row, rowIndex) =>
+        row.map(({ element, itemProps = {} }, itemIndex) => (
+          <Grid
+            key={`${rowIndex}-${itemIndex}`}
+            order={{
+              xs: getOrder(rowIndex, itemIndex, globalItemProps.xs),
+              sm: getOrder(rowIndex, itemIndex, globalItemProps.sm),
+              md: getOrder(rowIndex, itemIndex, globalItemProps.md),
+              lg: getOrder(rowIndex, itemIndex, globalItemProps.lg),
+              xl: getOrder(rowIndex, itemIndex, globalItemProps.xl)
+            }}
+            xsOffset={getOffset(itemIndex, globalItemProps.xs)}
+            smOffset={getOffset(itemIndex, globalItemProps.sm)}
+            mdOffset={getOffset(itemIndex, globalItemProps.md)}
+            lgOffset={getOffset(itemIndex, globalItemProps.lg)}
+            xlOffset={getOffset(itemIndex, globalItemProps.xl)}
+            {...globalItemProps}
+            {...itemProps}
+          >
+            {element}
+          </Grid>
+        ))
+      )}
+    </Grid>
+  );
+};
+
+export default OrderedGrid;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import ElevatedAppBar, { ElevatedAppBarProps } from './ElevatedAppBar';
 import Image, { ImageProps } from './Image';
 import ItemizedList, { ItemizedListProps } from './ItemizedList';
+import OrderedGrid, { OrderedGridProps } from './OrderedGrid';
 
 export {
   ElevatedAppBar, type ElevatedAppBarProps,
   Image, type ImageProps,
-  ItemizedList, type ItemizedListProps
+  ItemizedList, type ItemizedListProps,
+  OrderedGrid, type OrderedGridProps
 };
