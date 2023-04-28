0b0722825ea434ac3fb949ea66e4bca4a4944751
SKairinos <stefan.kairinos@ocado.com>
2023-04-28 16:19:11 +0100

    fix: remove imports that break babel

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-// other
 import theme, { PropsColorOverrides } from './theme';
 
-// modules
-export * as components from './components';
-export * as helpers from './helpers';
-
-// exports
 export {
   theme,
   type PropsColorOverrides
