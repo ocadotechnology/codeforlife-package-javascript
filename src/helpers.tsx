9ed3886d93eb5e040ebf2d1f8f491b44410b2b08
SKairinos <stefan.kairinos@ocado.com>
2023-05-20 17:48:07 +0100

    fix: wrap function

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -118,3 +118,25 @@ export function overrideComponentsInTheme(
 
   return createTheme(theme, { components });
 };
+
+export function wrap(
+  newFn: {
+    before?: (...args: any[]) => void,
+    after?: (...args: any[]) => void
+  },
+  fn?: (...args: any[]) => any
+): (...args: any[]) => any {
+  return (...args) => {
+    if (newFn.before !== undefined) {
+      newFn.before(...args);
+    }
+    let value;
+    if (fn !== undefined) {
+      value = fn(...args);
+    }
+    if (newFn.after !== undefined) {
+      newFn.after(...args);
+    }
+    return value;
+  };
+}
