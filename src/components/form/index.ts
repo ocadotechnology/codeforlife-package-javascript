574f271648550c85b056b40fc8f67e0b0dcef552
SKairinos <skairinos@gmail.com>
2023-06-01 10:31:58 +0100

    fix: imports

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,7 +2,6 @@ import AutocompleteField, { AutocompleteFieldProps } from './AutocompleteField';
 import CheckboxField, { CheckboxFieldProps } from './CheckboxField';
 import EmailField, { EmailFieldProps } from './EmailField';
 import Form, { FormProps } from './Form';
-import MultiValueTextField, { MultiValueTextFieldProps } from './MultiValueTextField';
 import NewPasswordField, { NewPasswordFieldProps } from './NewPasswordField';
 import SubmitButton, { SubmitButtonProps } from './SubmitButton';
 import TextField, { TextFieldProps } from './TextField';
@@ -12,7 +11,6 @@ export {
   CheckboxField, type CheckboxFieldProps,
   EmailField, type EmailFieldProps,
   Form, type FormProps,
-  MultiValueTextField, type MultiValueTextFieldProps,
   NewPasswordField, type NewPasswordFieldProps,
   SubmitButton, type SubmitButtonProps,
   TextField, type TextFieldProps
