2b8663f731e23e2e9f0be07850e46dfee7585a24
SKairinos <skairinos@gmail.com>
2023-06-05 13:36:55 +0100

    fix: no padding on MuiContainer

--- src/theme/components/MuiContainer.ts
+++ src/theme/components/MuiContainer.ts
@@ -3,7 +3,8 @@ import Components, { getFlexStyleOverrides } from './_components';
 const MuiContainer: Components['MuiContainer'] = {
   styleOverrides: {
     root: ({ ownerState }) => ({
-      ...getFlexStyleOverrides(ownerState)
+      ...getFlexStyleOverrides(ownerState),
+      padding: '0px !important'
     })
   }
 };
