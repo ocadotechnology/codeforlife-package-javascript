ce194b2ebf7ad7f9fb1444818d9ac506ff4a5da8
SKairinos <skairinos@gmail.com>
2023-06-05 18:46:08 +0100

    feat: App component

---
+++ src/app/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Action,
+  AnyAction,
+  Store
+} from 'redux';
+import {
+  Theme,
+  ThemeProvider,
+  CssBaseline
+} from '@mui/material';
+
+export interface AppProps<
+  A extends Action = AnyAction,
+  S = unknown
+> {
+  theme: Theme;
+  store: Store<S, A>;
+  children: React.ReactNode;
+}
+
+const App = <
+  A extends Action = AnyAction,
+  S = unknown
+>({
+  theme,
+  store,
+  children
+}: AppProps<A, S>): JSX.Element => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      {children}
+    </Provider>
+  </ThemeProvider>
+);
+
+export default App;
