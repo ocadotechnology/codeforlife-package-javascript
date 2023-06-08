55506eb9fedd64f8ec4e84f10a770a2713babad9
SKairinos <skairinos@gmail.com>
2023-06-08 17:09:56 +0100

    fix: remove line-height

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,60 +1,64 @@
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 
+// TODO: assess if line-height is needed.
+//  Doesn't play well with responsiveFontSizes.
+//  https://mui.com/material-ui/customization/theming/#responsivefontsizes-theme-options-theme
+
 const typography: TypographyOptions = {
   h1: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '60px',
-    lineHeight: '60px',
+    // lineHeight: '60px',
     marginBottom: '24px'
   },
   h2: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '55px',
-    lineHeight: '55px',
+    // lineHeight: '55px',
     marginBottom: '22px'
   },
   h3: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '45px',
-    lineHeight: '47px',
+    // lineHeight: '47px',
     marginBottom: '20px'
   },
   h4: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '30px',
-    lineHeight: '38px',
+    // lineHeight: '38px',
     marginBottom: '18px'
   },
   h5: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '25px',
-    lineHeight: '32px',
+    // lineHeight: '32px',
     marginBottom: '16px'
   },
   h6: {
     fontFamily: '"SpaceGrotesk"',
     fontWeight: 500,
     fontSize: '22px',
-    lineHeight: '26px',
+    // lineHeight: '26px',
     marginBottom: '14px'
   },
   body1: {
     fontFamily: '"Inter"',
     fontWeight: 400,
     fontSize: '18px',
-    lineHeight: '22px',
+    // lineHeight: '22px',
     marginBottom: '16px'
   },
   body2: {
     fontFamily: '"Inter"',
     fontWeight: 400,
     fontSize: '16px',
-    lineHeight: '20px',
+    // lineHeight: '20px',
     marginBottom: '14px'
   },
   button: {
