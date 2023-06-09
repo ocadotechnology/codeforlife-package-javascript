75c50faa7a90032c738d8aae0473b4794233c36d
SKairinos <skairinos@gmail.com>
2023-06-09 20:00:21 +0100

    fix: don't load external scripts on dev

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -33,50 +33,52 @@ const App = <
   store,
   children
 }: AppProps<A, S>): JSX.Element => {
-  const oneTrustEventTypes = [
-    useExternalScript({
-      props: {
-        src: 'https://cdn-ukwest.onetrust.com/consent/5da42396-cb12-4493-8d04-5179033cfbad/OtAutoBlock.js',
-        type: 'text/javascript'
-      },
-      eventTypes: ['load', 'error']
-    }),
-    useExternalScript({
-      props: {
-        src: 'https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js',
-        type: 'text/javascript',
-        charset: 'UTF-8'
-      },
-      attrs: {
-        'data-domain-script': '5da42396-cb12-4493-8d04-5179033cfbad'
-      },
-      eventTypes: ['load', 'error']
-    })
-  ];
-  if (oneTrustEventTypes.some(t => t === 'error')) {
-    alert('OneTrust failed to load!');
-  }
+  if (process.env.NODE_ENV !== 'development') {
+    const oneTrustEventTypes = [
+      useExternalScript({
+        props: {
+          src: 'https://cdn-ukwest.onetrust.com/consent/5da42396-cb12-4493-8d04-5179033cfbad/OtAutoBlock.js',
+          type: 'text/javascript'
+        },
+        eventTypes: ['load', 'error']
+      }),
+      useExternalScript({
+        props: {
+          src: 'https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js',
+          type: 'text/javascript',
+          charset: 'UTF-8'
+        },
+        attrs: {
+          'data-domain-script': '5da42396-cb12-4493-8d04-5179033cfbad'
+        },
+        eventTypes: ['load', 'error']
+      })
+    ];
+    if (oneTrustEventTypes.some(t => t === 'error')) {
+      alert('OneTrust failed to load!');
+    }
 
-  // @ts-expect-error required by freshworks script.
-  window.fwSettings = {
-    widget_id: 77000000397
-  };
-  const freshworksEventType = useExternalScript({
-    props: {
-      src: 'https://euc-widget.freshworks.com/widgets/77000000397.js',
-      type: 'text/javascript',
-      async: true,
-      defer: true
-    },
-    eventTypes: ['load', 'error']
-  });
-  if (freshworksEventType === 'load') {
-    // TODO: figure out better approach.
-    setTimeout(() => {
-      useFreshworksWidget('hide');
-    }, 1000);
-  } else if (freshworksEventType === 'error') {
-    alert('Freshworks failed to load!');
+    // @ts-expect-error required by freshworks script.
+    window.fwSettings = {
+      widget_id: 77000000397
+    };
+    const freshworksEventType = useExternalScript({
+      props: {
+        src: 'https://euc-widget.freshworks.com/widgets/77000000397.js',
+        type: 'text/javascript',
+        async: true,
+        defer: true
+      },
+      eventTypes: ['load', 'error']
+    });
+    if (freshworksEventType === 'load') {
+      // TODO: figure out better approach.
+      setTimeout(() => {
+        useFreshworksWidget('hide');
+      }, 1000);
+    } else if (freshworksEventType === 'error') {
+      alert('Freshworks failed to load!');
+    }
   }
 
   return (
