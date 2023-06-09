1bd28aef3ea0e732c3b4c2aaa4a04601c1de55d6
SKairinos <skairinos@gmail.com>
2023-06-09 19:28:48 +0100

    feat: custom hooks and load external scripts in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,6 +11,8 @@ import {
   CssBaseline
 } from '@mui/material';
 
+import { useExternalScript } from '../hooks';
+
 export interface AppProps<
   A extends Action = AnyAction,
   S = unknown
@@ -27,19 +29,62 @@ const App = <
   theme,
   store,
   children
-}: AppProps<A, S>): JSX.Element => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <style>{`
+}: AppProps<A, S>): JSX.Element => {
+  const oneTrustEventTypes = [
+    useExternalScript({
+      props: {
+        src: 'https://cdn-ukwest.onetrust.com/consent/5da42396-cb12-4493-8d04-5179033cfbad/OtAutoBlock.js',
+        type: 'text/javascript'
+      },
+      eventTypes: ['load', 'error']
+    }),
+    useExternalScript({
+      props: {
+        src: 'https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js',
+        type: 'text/javascript',
+        charset: 'UTF-8'
+      },
+      attrs: {
+        'data-domain-script': '5da42396-cb12-4493-8d04-5179033cfbad'
+      },
+      eventTypes: ['load', 'error']
+    })
+  ];
+  if (oneTrustEventTypes.some(t => t === 'error')) {
+    alert('OneTrust failed to load!');
+  }
+
+  // @ts-expect-error required by freshworks script.
+  window.fwSettings = {
+    widget_id: 77000000397
+  };
+  const freshworksEventType = useExternalScript({
+    props: {
+      src: 'https://euc-widget.freshworks.com/widgets/77000000397.js',
+      type: 'text/javascript',
+      async: true,
+      defer: true
+    },
+    eventTypes: ['load', 'error']
+  });
+  if (freshworksEventType === 'error') {
+    alert('Freshworks failed to load!');
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <style>{`
       body {
         margin: 0px;
         padding: 0px;
       }
     `}</style>
-    <Provider store={store}>
-      {children}
-    </Provider>
-  </ThemeProvider>
-);
+      <Provider store={store}>
+        {children}
+      </Provider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/hooks.ts
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+
+export function useFreshworksWidget(display: 'open' | 'hide'): void {
+  // @ts-expect-error defined in external script
+  window.FreshworksWidget(display);
+}
+
+export function useOneTrustInfoToggle(): void {
+  // @ts-expect-error defined in external script
+  window.Optanon.ToggleInfoDisplay();
+}
+
+export function useExternalScript<
+  EventType extends keyof HTMLElementEventMap
+>({
+  props,
+  attrs,
+  eventTypes
+}: {
+  props: Partial<HTMLScriptElement> & { src: string; };
+  attrs?: Record<string, string>;
+  eventTypes?: EventType[];
+}): EventType | undefined {
+  const [eventType, setEventType] = useState<EventType>();
+
+  useEffect(() => {
+    let script = document.querySelector<HTMLScriptElement>(
+      `script[src="${props.src}"]`
+    );
+
+    if (script === null) {
+      script = document.createElement('script');
+
+      Object.entries(props).forEach(([key, value]) => {
+        // @ts-expect-error key is type checked
+        script[key] = value;
+      });
+
+      if (attrs !== undefined) {
+        Object.entries(attrs).forEach(([key, value]) => {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          script!.setAttribute(key, value);
+        });
+      }
+    }
+
+    function eventListener(event: Event): void {
+      setEventType(event.type as EventType);
+    };
+
+    eventTypes?.forEach((eventType) => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      script!.addEventListener(eventType, eventListener);
+    });
+
+    document.head.appendChild(script);
+
+    return () => {
+      eventTypes?.forEach((eventType) => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        script!.removeEventListener(eventType, eventListener);
+      });
+    };
+  }, []);
+
+  return eventType;
+}
