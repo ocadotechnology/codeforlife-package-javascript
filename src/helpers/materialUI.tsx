451ad206a4c2d74fbb50ab364322d0d1d67e05c2
SKairinos <skairinos@gmail.com>
2023-06-12 19:06:08 +0100

    replaced by getStyleOverrides helper

--- src/helpers/materialUI.tsx
+++ src/helpers/materialUI.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import {
   Divider,
   DividerProps,
-  useTheme,
-  createTheme,
-  Theme,
-  Components,
-  ComponentsPropsList,
   ThemeOptions
 } from '@mui/material';
 import { CommonProps } from '@mui/material/OverridableComponent';
@@ -27,62 +22,6 @@ export function insertDividerBetweenElements({
       : undefined
     }
   </>);
-}
-
-export function overrideComponentsInTheme(
-  componentOverrides: Components<Omit<Theme, 'components'>>,
-  theme?: Theme
-): Theme {
-  theme = theme ?? useTheme<Theme>();
-
-  let components = theme.components ?? {};
-  components = {
-    ...components,
-    ...Object.fromEntries(Object.keys(componentOverrides)
-      .map((key: keyof Components<Omit<Theme, 'components'>>) => {
-        const component = components[key] ?? {};
-        const componentOverride = componentOverrides[key] ?? {};
-
-        interface RootFunctionArgs {
-          ownerState: ComponentsPropsList[typeof key]
-        }
-        interface StyleOverrides {
-          root?: object | (({ ownerState }: RootFunctionArgs) => object)
-        }
-
-        const componentStyleOverrides: StyleOverrides = ('styleOverrides' in component)
-          ? component.styleOverrides as object
-          : {};
-        const componentOverrideStyleOverrides: StyleOverrides = ('styleOverrides' in componentOverride)
-          ? componentOverride.styleOverrides as object
-          : {};
-
-        return [key, {
-          ...component,
-          ...componentOverride,
-          styleOverrides: {
-            ...componentStyleOverrides,
-            ...componentOverrideStyleOverrides,
-            root: ({ ownerState }: RootFunctionArgs) => ({
-              ...(typeof componentStyleOverrides.root === 'function' &&
-                componentStyleOverrides.root({ ownerState })
-              ),
-              ...(typeof componentStyleOverrides.root === 'object' &&
-                componentStyleOverrides.root
-              ),
-              ...(typeof componentOverrideStyleOverrides.root === 'function' &&
-                componentOverrideStyleOverrides.root({ ownerState })
-              ),
-              ...(typeof componentOverrideStyleOverrides.root === 'object' &&
-                componentOverrideStyleOverrides.root
-              )
-            })
-          }
-        }];
-      }))
-  };
-
-  return createTheme(theme, { components });
 }
 
 export function getStyleOverrides(
