39b39c0f78de7384b71e588e008d15294c753c51
SKairinos <skairinos@gmail.com>
2023-06-15 13:15:04 +0100

    fix: use negative margin

--- src/theme/components/MuiCheckbox.ts
+++ src/theme/components/MuiCheckbox.ts
@@ -1,17 +1,9 @@
-import { touchRippleClasses } from '@mui/material';
-
 import Components from './_components';
 
 const MuiCheckbox: Components['MuiCheckbox'] = {
   styleOverrides: {
     root: {
-      paddingLeft: 0,
-      [`.${touchRippleClasses.root}`]: {
-        left: '-12px'
-      },
-      [`.${touchRippleClasses.root} > *`]: {
-        left: '-12px'
-      }
+      marginLeft: '-9px'
     }
   }
 };
