3bfb967ce7af4ef80eb2664b1ad833937ba54eb0
SKairinos <skairinos@gmail.com>
2023-06-15 13:55:29 +0100

    fix: default styles

--- src/theme/components/MuiLink.ts
+++ src/theme/components/MuiLink.ts
@@ -1,29 +1,27 @@
-import Components, {
-  getFontStyleOverrides
-} from './_components';
-
 import { includesClassNames } from '../../helpers';
+import typography from '../typography';
+import Components, { getFontStyleOverrides } from './_components';
 
 const MuiLink: Components['MuiLink'] = {
-  defaultProps: {
-    underline: 'hover'
-  },
   styleOverrides: {
     root: ({ ownerState }) => ({
+      ...typography.body1,
+      marginBottom: 0,
       ...getFontStyleOverrides(ownerState),
       cursor: 'pointer',
+      textDecoration: 'underline',
+      ':hover': {
+        fontWeight: 'bold'
+      },
       ...(includesClassNames(ownerState, ['back-to']) && {
-        color: 'black !important',
+        ...typography.body1,
+        textDecoration: 'none',
+        ':hover': {
+          fontWeight: 'bold',
+          textDecoration: 'underline'
+        },
         ':before': {
           content: '"< Back to "'
-        }
-      }),
-      ...(includesClassNames(ownerState, ['body']) && {
-        color: 'black',
-        textDecorationColor: 'black',
-        textDecoration: 'underline',
-        ':hover': {
-          fontWeight: 'bold'
         }
       })
     })
