d4278816204dab05b8cb5c9068a15fa66fac5002
SKairinos <skairinos@gmail.com>
2023-06-16 19:17:02 +0100

    feat: SelectButton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ import ElevatedAppBar, { ElevatedAppBarProps } from './ElevatedAppBar';
 import Image, { ImageProps } from './Image';
 import ItemizedList, { ItemizedListProps } from './ItemizedList';
 import OrderedGrid, { OrderedGridProps } from './OrderedGrid';
+import SelectButton, { SelectButtonProps } from './SelectButton';
 import YouTubeVideo, { YouTubeVideoProps } from './YouTubeVideo';
 
 export {
@@ -15,5 +16,6 @@ export {
   Image, type ImageProps,
   ItemizedList, type ItemizedListProps,
   OrderedGrid, type OrderedGridProps,
+  SelectButton, type SelectButtonProps,
   YouTubeVideo, type YouTubeVideoProps
 };
