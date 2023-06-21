eb56c1e6273c78e2dcf548bab691305edee95b56
SKairinos <skairinos@gmail.com>
2023-06-21 17:21:42 +0100

    fix: default props

--- src/theme/components/MuiDialog.tsx
+++ src/theme/components/MuiDialog.tsx
@@ -1,6 +1,9 @@
 import Components from './_components';
 
 const MuiDialog: Components['MuiDialog'] = {
+  defaultProps: {
+    maxWidth: 'sm'
+  },
   styleOverrides: {
     paper: {
       borderRadius: '0px !important',
