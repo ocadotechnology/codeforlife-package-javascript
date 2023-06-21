59329d6f7d55d1c79a3859bdcba56a688ebe33ca
SKairinos <skairinos@gmail.com>
2023-06-21 18:01:28 +0100

    fix: svgs as strings

--- types/fixes.d.ts
+++ types/fixes.d.ts
@@ -6,6 +6,11 @@ declare namespace React {
   type StatelessComponent<P> = React.FunctionComponent<P>;
 }
 
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
 /*
  * Don't care about the typings of external libraries.
  * All libraries without typings will be imported as `any`.
