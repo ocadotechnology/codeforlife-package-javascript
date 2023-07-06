b658664033ea96c12659df63dcefe84852d690e8
SKairinos <skairinos@gmail.com>
2023-07-06 13:39:36 +0100

    fix: configure margin bottom

--- src/components/form/CheckboxField.tsx
+++ src/components/form/CheckboxField.tsx
@@ -26,15 +26,17 @@ import { form as formTypography } from '../../theme/typography';
 import ClickableTooltip from '../ClickableTooltip';
 
 export interface CheckboxFieldProps extends Omit<CheckboxProps, 'defaultValue'> {
-  formControlLabelProps: Omit<FormControlLabelProps, 'control'>,
-  validate?: FieldValidator | BooleanSchema,
-  name: string
+  formControlLabelProps: Omit<FormControlLabelProps, 'control'>;
+  validate?: FieldValidator | BooleanSchema;
+  marginBottom?: number | string;
+  name: string;
 }
 
 const CheckboxField: React.FC<CheckboxFieldProps> = ({
   formControlLabelProps,
   validate = YupBool(),
   required = false,
+  marginBottom = formTypography.marginBottom,
   name,
   onChange,
   ...otherCheckboxProps
@@ -79,7 +81,7 @@ const CheckboxField: React.FC<CheckboxFieldProps> = ({
             direction='row'
             alignItems='center'
             marginLeft='-12px'
-            marginBottom={formTypography.marginBottom}
+            marginBottom={marginBottom}
             gap={1}
           >
             <FormControlLabel
