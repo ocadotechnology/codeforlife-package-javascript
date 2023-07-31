e0605436f5e1817c6e2ba4d581a9f510694c70ba
Stefan Kairinos <118008817+SKairinos@users.noreply.github.com>
2023-07-31 14:09:32 +0300

    fix: bgcolor (#23)
    * fix: bgcolor
    
    * push build files [skip ci]
    
    Co-Authored-By: cfl-bot <codeforlife-bot@ocado.com>

--- lib/esm/theme/ThemedBox.d.ts
+++ lib/esm/theme/ThemedBox.d.ts
@@ -4,6 +4,7 @@ export interface ThemedBoxProps extends BoxProps {
     options?: ThemeOptions;
     withShapes?: boolean;
     userType: 'teacher' | 'student' | 'independent';
+    bgcolor?: string;
 }
 declare const ThemedBox: React.FC<ThemedBoxProps>;
 export default ThemedBox;
