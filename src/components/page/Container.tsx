0d5e52816b1250291b1a7a6ffaee88e3364587ae
SKairinos <skairinos@gmail.com>
2023-07-31 13:04:23 +0300

    feat: include notifications on page navigation

--- src/components/page/Container.tsx
+++ src/components/page/Container.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   Unstable_Grid2 as Grid,
   Grid2Props
 } from '@mui/material';
+
+import Notification, { NotificationProps } from './Notification';
 
 export interface ContainerProps extends Omit<Grid2Props, (
   'id' |
   'container'
 )> { }
 
-const Container: React.FC<ContainerProps> = (props) => {
+const Container: React.FC<ContainerProps> = ({
+  children,
+  ...otherGridProps
+}) => {
+  const location = useLocation();
+  const childrenArray = React.Children.toArray(children);
+
+  if (location.state !== null &&
+    Array.isArray(location.state.notifications)
+  ) {
+    (location.state.notifications as Array<{
+      props: NotificationProps;
+      index?: number;
+    }>)
+      .filter((notification) => 'props' in notification)
+      .forEach((notification, index) => {
+        childrenArray.splice(
+          notification.index ?? index,
+          0,
+          <Notification {...notification.props} />
+        );
+      });
+  }
+
   return (
     <Grid
       id='body'
       container
-      {...props}
-    />
+      {...otherGridProps}
+    >
+      {childrenArray}
+    </Grid>
   );
 };
 
