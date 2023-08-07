6a86f5faf6854ba66cbc8cb290497a773427db45
KamilPawel <70412950+KamilPawel@users.noreply.github.com>
2023-08-07 13:34:09 +0100

    fix: disable ripple (#24)
    * fix: disable ripple animation on the tabs
    
    * push build files [skip ci]
    
    * fix: eslint
    
    Co-Authored-By: cfl-bot <codeforlife-bot@ocado.com>

--- src/components/page/TabBar.tsx
+++ src/components/page/TabBar.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  useParams,
-  useNavigate,
-  generatePath
-} from 'react-router-dom';
+import { useParams, useNavigate, generatePath } from 'react-router-dom';
 import {
   Tabs,
   Tab,
@@ -15,10 +11,7 @@ import {
   ChevronLeft as ChevronLeftIcon,
   ChevronRight as ChevronRightIcon
 } from '@mui/icons-material';
-import {
-  object as YupObject,
-  string as YupString
-} from 'yup';
+import { object as YupObject, string as YupString } from 'yup';
 
 import { primary } from '../../theme/colors';
 import { tryValidateSync } from '../../helpers/yup';
@@ -44,91 +37,100 @@ const TabBar: React.FC<TabBarProps> = ({
   const params = useParams();
   const navigate = useNavigate();
   const [_value, _setValue] = React.useState(
-    value < 0
-      ? 0
-      : value >= tabs.length
-        ? tabs.length - 1
-        : value
+    value < 0 ? 0 : value >= tabs.length ? tabs.length - 1 : value
   );
 
-  const labels = tabs.map(tab => tab.label);
-  const children = tabs.map(tab => tab.children);
-  const paths = tabs.map(tab => tab.path);
+  const labels = tabs.map((tab) => tab.label);
+  const children = tabs.map((tab) => tab.children);
+  const paths = tabs.map((tab) => tab.path);
 
   React.useEffect(() => {
     _setValue(value);
   }, [value]);
 
   React.useEffect(() => {
-    const tab = tryValidateSync(params, YupObject({
-      tab: YupString().oneOf(paths).required()
-    }))?.tab;
+    const tab = tryValidateSync(
+      params,
+      YupObject({
+        tab: YupString().oneOf(paths).required()
+      })
+    )?.tab;
 
     if (tab !== undefined) {
       _setValue(paths.indexOf(tab));
     }
   }, [params]);
 
-  return <>
-    <Section
-      gridProps={{ bgcolor: primary[500] }}
-      sx={{ paddingY: '100px' }}
-      className='flex-center'
-    >
-      <Typography
-        textAlign='center'
-        variant='h2'
-        style={{ color: 'white' }}
-        mb={0}
+  return (
+    <>
+      <Section
+        gridProps={{ bgcolor: primary[500] }}
+        sx={{ paddingY: '100px' }}
+        className="flex-center"
       >
-        {header}
-      </Typography>
-    </Section>
-    <Section
-      gridProps={{ bgcolor: primary[300] }}
-      sx={{ paddingY: '6px' }}
-      className='flex-center'
-    >
-      <Tabs
-        value={_value}
-        onChange={(_, value) => {
-          navigate(generatePath(originalPath, {
-            tab: paths[value]
-          }));
-        }}
-        ScrollButtonComponent={({
-          disabled,
-          onClick,
-          direction
-        }: TabScrollButtonProps) => {
-          return <>{disabled === false && (
-            // @ts-expect-error button props not required
-            <IconButton
-              onClick={onClick}
-              style={{
-                padding: 0,
-                [direction === 'left'
-                  ? 'marginRight'
-                  : 'marginLeft'
-                ]: '15px',
-                color: 'white'
-              }}
-            >
-              {direction === 'left'
-                ? <ChevronLeftIcon />
-                : <ChevronRightIcon />
-              }
-            </IconButton>
-          )}</>;
-        }}
+        <Typography
+          textAlign="center"
+          variant="h2"
+          style={{ color: 'white' }}
+          mb={0}
+        >
+          {header}
+        </Typography>
+      </Section>
+      <Section
+        gridProps={{ bgcolor: primary[300] }}
+        sx={{ paddingY: '6px' }}
+        className="flex-center"
       >
-        {labels.map((label) =>
-          <Tab key={label} label={label} />
-        )}
-      </Tabs>
-    </Section>
-    {children[_value]}
-  </>;
+        <Tabs
+          value={_value}
+          onChange={(_, value) => {
+            navigate(
+              generatePath(originalPath, {
+                tab: paths[value]
+              })
+            );
+          }}
+          ScrollButtonComponent={({
+            disabled,
+            onClick,
+            direction
+          }: TabScrollButtonProps) => {
+            return (
+              <>
+                {disabled === false && (
+                  // @ts-expect-error button props not required
+                  <IconButton
+                    onClick={onClick}
+                    style={{
+                      padding: 0,
+                      [direction === 'left' ? 'marginRight' : 'marginLeft']:
+                        '15px',
+                      color: 'white'
+                    }}
+                  >
+                    {direction === 'left'
+                      ? <>
+                        <ChevronLeftIcon />
+                      </>
+                      : <>
+                        <ChevronRightIcon />
+                      </>
+                    }
+                  </IconButton>
+                )}
+              </>
+            );
+          }}
+        >
+          {labels.map((label) => (
+            <Tab disableRipple key={label} label={label} />
+          ))}
+        </Tabs>
+      </Section>
+      {children[_value]}
+    </>
+  );
 };
 
 export default TabBar;
