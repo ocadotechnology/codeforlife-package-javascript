8a74aa82f74053d685cf02e48ab823777826b728
SKairinos <skairinos@gmail.com>
2023-09-07 18:38:53 +0100

    simplify api index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,65 +1,23 @@
-import {
-  reactHooksModuleName
-} from '@reduxjs/toolkit/dist/query/react/module';
-import {
-  Api,
-  BaseQueryFn,
-  CreateApiOptions,
-  EndpointDefinitions,
-  FetchArgs,
-  FetchBaseQueryError,
-  createApi as _createApi
-} from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 
-import _baseQuery from './baseQuery';
+import baseQuery from './baseQuery';
 
-type FetchBaseQuery = BaseQueryFn<FetchArgs, unknown, FetchBaseQueryError>;
-type GlobalTagTypes = (
-  'user' |
-  'school' |
-  'class' |
-  'teacher' |
+export const TAG_TYPES = [
+  'user',
+  'school',
+  'class',
+  'teacher',
   'student'
-);
+] as const;
 
-export default function createApi<
-  Definitions extends EndpointDefinitions,
-  ReducerPath extends string = 'api',
-  TagTypes extends string = never
->({
-  reducerPath = 'api' as ReducerPath,
-  baseQuery = _baseQuery,
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  endpoints = () => ({}) as Definitions,
-  tagTypes = [],
-  ...otherOptions
-}: Partial<CreateApiOptions<
-  FetchBaseQuery,
-  Definitions,
-  ReducerPath,
-  TagTypes | GlobalTagTypes
->>): Api<
-  FetchBaseQuery,
-  Definitions,
-  ReducerPath,
-  TagTypes | GlobalTagTypes,
-  (
-    typeof import('@reduxjs/toolkit/dist/query/core/module').coreModuleName |
-    typeof reactHooksModuleName
-  )
-> {
-  return _createApi({
-    reducerPath,
-    baseQuery,
-    endpoints,
-    tagTypes: [
-      ...tagTypes,
-      'user',
-      'school',
-      'class',
-      'teacher',
-      'student'
-    ],
-    ...otherOptions
-  });
+const api = createApi({
+  reducerPath: 'api',
+  baseQuery,
+  endpoints: () => ({}),
+  tagTypes: TAG_TYPES
+});
+
+export default api;
+export {
+  baseQuery
 };
