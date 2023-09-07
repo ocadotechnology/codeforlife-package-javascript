cd34e06ec0348d0775a522776c0f718bb73864b9
SKairinos <skairinos@gmail.com>
2023-09-07 18:12:46 +0100

    comment out helper

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,65 +1,65 @@
-import {
-  reactHooksModuleName
-} from '@reduxjs/toolkit/dist/query/react/module';
-import {
-  Api,
-  BaseQueryFn,
-  CreateApiOptions,
-  EndpointDefinitions,
-  FetchArgs,
-  FetchBaseQueryError,
-  createApi as _createApi
-} from '@reduxjs/toolkit/query/react';
+// import {
+//   reactHooksModuleName
+// } from '@reduxjs/toolkit/dist/query/react/module';
+// import {
+//   Api,
+//   BaseQueryFn,
+//   CreateApiOptions,
+//   EndpointDefinitions,
+//   FetchArgs,
+//   FetchBaseQueryError,
+//   createApi as _createApi
+// } from '@reduxjs/toolkit/query/react';
 
-import _baseQuery from './baseQuery';
+// import _baseQuery from './baseQuery';
 
-type FetchBaseQuery = BaseQueryFn<FetchArgs, unknown, FetchBaseQueryError>;
-type GlobalTagTypes = (
-  'user' |
-  'school' |
-  'class' |
-  'teacher' |
-  'student'
-);
+// type FetchBaseQuery = BaseQueryFn<FetchArgs, unknown, FetchBaseQueryError>;
+// type GlobalTagTypes = (
+//   'user' |
+//   'school' |
+//   'class' |
+//   'teacher' |
+//   'student'
+// );
 
-export default function createApi<
-  Definitions extends EndpointDefinitions,
-  ReducerPath extends string = 'api',
-  TagTypes extends string = never
->({
-  reducerPath = 'api' as ReducerPath,
-  baseQuery = _baseQuery,
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  endpoints = () => ({}) as Definitions,
-  tagTypes = [],
-  ...otherOptions
-}: Partial<CreateApiOptions<
-  FetchBaseQuery,
-  Definitions,
-  ReducerPath,
-  TagTypes | GlobalTagTypes
->>): Api<
-  FetchBaseQuery,
-  Definitions,
-  ReducerPath,
-  TagTypes | GlobalTagTypes,
-  (
-    typeof import('@reduxjs/toolkit/dist/query/core/module').coreModuleName |
-    typeof reactHooksModuleName
-  )
-> {
-  return _createApi({
-    reducerPath,
-    baseQuery,
-    endpoints,
-    tagTypes: [
-      ...tagTypes,
-      'user',
-      'school',
-      'class',
-      'teacher',
-      'student'
-    ],
-    ...otherOptions
-  });
-};
+// export default function createApi<
+//   Definitions extends EndpointDefinitions,
+//   ReducerPath extends string = 'api',
+//   TagTypes extends string = never
+// >({
+//   reducerPath = 'api' as ReducerPath,
+//   baseQuery = _baseQuery,
+//   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+//   endpoints = () => ({}) as Definitions,
+//   tagTypes = [],
+//   ...otherOptions
+// }: Partial<CreateApiOptions<
+//   FetchBaseQuery,
+//   Definitions,
+//   ReducerPath,
+//   TagTypes | GlobalTagTypes
+// >>): Api<
+//   FetchBaseQuery,
+//   Definitions,
+//   ReducerPath,
+//   TagTypes | GlobalTagTypes,
+//   (
+//     typeof import('@reduxjs/toolkit/dist/query/core/module').coreModuleName |
+//     typeof reactHooksModuleName
+//   )
+// > {
+//   return _createApi({
+//     reducerPath,
+//     baseQuery,
+//     endpoints,
+//     tagTypes: [
+//       ...tagTypes,
+//       'user',
+//       'school',
+//       'class',
+//       'teacher',
+//       'student'
+//     ],
+//     ...otherOptions
+//   });
+// };
