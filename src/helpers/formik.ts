52f00dc69357578a96236d5644eb87c8fe594fe0
SKairinos <skairinos@gmail.com>
2023-09-20 17:30:45 +0100

    fix: error join

--- src/helpers/formik.ts
+++ src/helpers/formik.ts
@@ -1,8 +1,8 @@
+import { MutationDefinition } from '@reduxjs/toolkit/dist/query';
+import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 import {
   FormikHelpers
 } from 'formik';
-import { MutationDefinition } from '@reduxjs/toolkit/dist/query';
-import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 
 export function setFormErrors(
   error: unknown,
@@ -20,7 +20,7 @@ export function setFormErrors(
 
   const data = Object.fromEntries(
     Object.entries(error.data).map(([field, errors]) => {
-      if (Array.isArray(errors)) errors = errors.join(', ');
+      if (Array.isArray(errors)) errors = errors.join('. ');
       return [field, errors];
     })
   );
