d593a4de29b8971bd4de95e863d0699f6dd1c977
SKairinos <stefan.kairinos@ocado.com>
2024-06-03 15:24:40 +0000

    delete lib files

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,3 @@
-export const SERVICE_NAME = process.env.REACT_APP_SERVICE_NAME ?? "REPLACE_ME"
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ?? "http://localhost:8000/api/"
-export const PORTAL_BASE_URL =
-  process.env.REACT_APP_PORTAL_BASE_URL ?? "http://localhost:3000"
+export const SERVICE_NAME = import.meta.env.VITE_SERVICE_NAME ?? "REPLACE_ME"
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000/api/"
+export const PORTAL_BASE_URL = import.meta.env.VITE_PORTAL_BASE_URL ?? "http://localhost:3000"
