e6f7039a783c2ec6e6b2e4b6deaee5dfc7774ed6
SKairinos <stefan.kairinos@ocado.com>
2024-06-04 11:15:18 +0000

    clean up, formatting, type helpers

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
 export const SERVICE_NAME = import.meta.env.VITE_SERVICE_NAME ?? "REPLACE_ME"
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000/api/"
-export const PORTAL_BASE_URL = import.meta.env.VITE_PORTAL_BASE_URL ?? "http://localhost:3000"
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000/api/"
+export const PORTAL_BASE_URL =
+  import.meta.env.VITE_PORTAL_BASE_URL ?? "http://localhost:3000"
