05cc88741d731cc860ef9095e5e37d49e904e314
SKairinos <stefan.kairinos@ocado.com>
2024-06-12 10:26:03 +0000

    comment out broken imports

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,9 @@
-import AutocompleteField, {
-  type AutocompleteFieldProps,
-} from "./AutocompleteField"
-import CheckboxField, { type CheckboxFieldProps } from "./CheckboxField"
-import DateField, { type DateFieldProps } from "./DateField"
+// TODO: figure out which import(s) are breaking bundler
+// import AutocompleteField, {
+//   type AutocompleteFieldProps,
+// } from "./AutocompleteField"
+// import CheckboxField, { type CheckboxFieldProps } from "./CheckboxField"
+// import DateField, { type DateFieldProps } from "./DateField"
 import EmailField, { type EmailFieldProps } from "./EmailField"
 import Form, { type FormErrors, type FormProps } from "./Form"
 import PasswordField, { type PasswordFieldProps } from "./PasswordField"
@@ -10,17 +11,17 @@ import SubmitButton, { type SubmitButtonProps } from "./SubmitButton"
 import TextField, { type TextFieldProps } from "./TextField"
 
 export {
-  AutocompleteField,
-  CheckboxField,
-  DateField,
+  // AutocompleteField,
+  // CheckboxField,
+  // DateField,
   EmailField,
   Form,
   PasswordField,
   SubmitButton,
   TextField,
-  type AutocompleteFieldProps,
-  type CheckboxFieldProps,
-  type DateFieldProps,
+  // type AutocompleteFieldProps,
+  // type CheckboxFieldProps,
+  // type DateFieldProps,
   type EmailFieldProps,
   type FormErrors,
   type FormProps,
