4d5e8ac6beaa30e390a95042400355269c3d688a
SKairinos <skairinos@gmail.com>
2024-06-27 08:38:24 +0000

    fix: repeat field

---
+++ src/components/form/RepeatField.tsx
@@ -0,0 +1,82 @@
+import { TextField, type TextFieldProps } from "@mui/material"
+import { Field, type FieldConfig, type FieldProps } from "formik"
+import { useState, type FC } from "react"
+import * as yup from "yup"
+
+import { schemaToFieldValidator } from "../../utils/form"
+
+export type RepeatFieldProps = Omit<
+  TextFieldProps,
+  | "name"
+  | "id"
+  | "value"
+  | "onChange"
+  | "onBlur"
+  | "error"
+  | "helperText"
+  | "defaultValue"
+  | "required"
+  | "type"
+> & {
+  name: string
+}
+
+// https://formik.org/docs/examples/with-material-ui
+const RepeatField: FC<RepeatFieldProps> = ({
+  name,
+  label,
+  placeholder,
+  ...otherTextFieldProps
+}) => {
+  const [value, setValue] = useState("")
+
+  const repeatName = `repeat_${name}`
+
+  const fieldConfig: FieldConfig = {
+    name: repeatName,
+    type: "text",
+    validate: schemaToFieldValidator(
+      yup
+        .string()
+        .required()
+        .test(
+          `matches-${name}`,
+          `does not match`,
+          repeatValue => value === repeatValue,
+        ),
+    ),
+  }
+
+  return (
+    <Field {...fieldConfig}>
+      {({ form }: FieldProps) => {
+        setValue(form.values[name])
+
+        return (
+          <TextField
+            required
+            type="text"
+            label={label ?? `Repeat ${name.replace("_", " ")}`}
+            placeholder={
+              placeholder ?? `Enter your ${name.replace("_", " ")} again`
+            }
+            id={repeatName}
+            name={repeatName}
+            value={form.values[repeatName]}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            error={form.touched[repeatName] && Boolean(form.errors[repeatName])}
+            helperText={
+              (form.touched[repeatName] && form.errors[repeatName]) as
+                | false
+                | string
+            }
+            {...otherTextFieldProps}
+          />
+        )
+      }}
+    </Field>
+  )
+}
+
+export default RepeatField
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -8,17 +8,19 @@ import FirstNameField, { type FirstNameFieldProps } from "./FirstNameField"
 import Form, { type FormErrors, type FormProps } from "./Form"
 import OtpField, { type OtpFieldProps } from "./OtpField"
 import PasswordField, { type PasswordFieldProps } from "./PasswordField"
+import RepeatField, { type RepeatFieldProps } from "./RepeatField"
 import SubmitButton, { type SubmitButtonProps } from "./SubmitButton"
 import TextField, { type TextFieldProps } from "./TextField"
 
 export {
-  // AutocompleteField,
   CheckboxField,
   EmailField,
   FirstNameField,
   Form,
   OtpField,
   PasswordField,
+  // AutocompleteField,
+  RepeatField,
   SubmitButton,
   TextField,
   type CheckboxFieldProps,
@@ -29,6 +31,7 @@ export {
   type FormProps,
   type OtpFieldProps,
   type PasswordFieldProps,
+  type RepeatFieldProps,
   type SubmitButtonProps,
   type TextFieldProps,
 }
