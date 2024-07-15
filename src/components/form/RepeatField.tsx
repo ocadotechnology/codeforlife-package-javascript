cfc69415d09ce557ca106effce0349ca19288aee
SKairinos <stefan.kairinos@ocado.com>
2024-07-15 16:09:44 +0000

    fix repeat field

--- src/components/form/RepeatField.tsx
+++ src/components/form/RepeatField.tsx
@@ -1,6 +1,12 @@
-import { TextField, type TextFieldProps } from "@mui/material"
+import { TextField as MuiTextField, type TextFieldProps } from "@mui/material"
 import { Field, type FieldConfig, type FieldProps } from "formik"
-import { useState, type FC } from "react"
+import {
+  useEffect,
+  useState,
+  type Dispatch,
+  type FC,
+  type SetStateAction,
+} from "react"
 import { string as YupString, type ValidateOptions } from "yup"
 
 import { schemaToFieldValidator } from "../../utils/form"
@@ -21,11 +27,51 @@ export type RepeatFieldProps = Omit<
   validateOptions?: ValidateOptions
 }
 
-// https://formik.org/docs/examples/with-material-ui
-const RepeatField: FC<RepeatFieldProps> = ({
+const TextField: FC<
+  RepeatFieldProps & {
+    repeatName: string
+    setValue: Dispatch<SetStateAction<string>>
+    fieldProps: FieldProps
+  }
+> = ({
+  repeatName,
+  setValue,
+  fieldProps,
   name,
   label,
   placeholder,
+  type,
+  ...otherTextFieldProps
+}) => {
+  const { form } = fieldProps
+
+  useEffect(() => {
+    setValue(form.values[name])
+  })
+
+  return (
+    <MuiTextField
+      required
+      type={type}
+      label={label ?? `Repeat ${name.replace("_", " ")}`}
+      placeholder={placeholder ?? `Enter your ${name.replace("_", " ")} again`}
+      id={repeatName}
+      name={repeatName}
+      value={form.values[repeatName]}
+      onChange={form.handleChange}
+      onBlur={form.handleBlur}
+      error={form.touched[repeatName] && Boolean(form.errors[repeatName])}
+      helperText={
+        (form.touched[repeatName] && form.errors[repeatName]) as false | string
+      }
+      {...otherTextFieldProps}
+    />
+  )
+}
+
+// https://formik.org/docs/examples/with-material-ui
+const RepeatField: FC<RepeatFieldProps> = ({
+  name,
   type = "text",
   validateOptions,
   ...otherTextFieldProps
@@ -51,32 +97,16 @@ const RepeatField: FC<RepeatFieldProps> = ({
 
   return (
     <Field {...fieldConfig}>
-      {({ form }: FieldProps) => {
-        setValue(form.values[name])
-
-        return (
-          <TextField
-            required
-            type={type}
-            label={label ?? `Repeat ${name.replace("_", " ")}`}
-            placeholder={
-              placeholder ?? `Enter your ${name.replace("_", " ")} again`
-            }
-            id={repeatName}
-            name={repeatName}
-            value={form.values[repeatName]}
-            onChange={form.handleChange}
-            onBlur={form.handleBlur}
-            error={form.touched[repeatName] && Boolean(form.errors[repeatName])}
-            helperText={
-              (form.touched[repeatName] && form.errors[repeatName]) as
-                | false
-                | string
-            }
-            {...otherTextFieldProps}
-          />
-        )
-      }}
+      {(fieldProps: FieldProps) => (
+        <TextField
+          name={name}
+          type={type}
+          repeatName={repeatName}
+          setValue={setValue}
+          fieldProps={fieldProps}
+          {...otherTextFieldProps}
+        />
+      )}
     </Field>
   )
 }
