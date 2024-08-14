d658fb007a06c3f86da76188513e7590ec2b26f3
SKairinos <skairinos@gmail.com>
2024-08-14 14:47:15 +0000

    fix env vars

--- src/env.ts
+++ src/env.ts
@@ -13,12 +13,12 @@ export default env
 export const SERVICE_NAME = env.VITE_SERVICE_NAME ?? "REPLACE_ME"
 
 // If the current service the root service. This will only be true for portal.
-export const SERVICE_IS_ROOT = Boolean(Number(env.SERVICE_IS_ROOT ?? "0"))
+export const SERVICE_IS_ROOT = Boolean(Number(env.VITE_SERVICE_IS_ROOT ?? "0"))
 
 // The protocol, domain and port of the current service.
-export const SERVICE_PROTOCOL = env.SERVICE_PROTOCOL ?? "http"
-export const SERVICE_DOMAIN = env.SERVICE_DOMAIN ?? "localhost"
-export const SERVICE_PORT = Number(env.SERVICE_PORT ?? "8000")
+export const SERVICE_PROTOCOL = env.VITE_SERVICE_PROTOCOL ?? "http"
+export const SERVICE_DOMAIN = env.VITE_SERVICE_DOMAIN ?? "localhost"
+export const SERVICE_PORT = Number(env.VITE_SERVICE_PORT ?? "8000")
 
 // The base url of the current service.
 // The root service does not need its name included in the base url.
