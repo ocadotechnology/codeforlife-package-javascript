899a7ecd78b29aa41dc70d816b7df57e74277053
SKairinos <skairinos@gmail.com>
2024-09-24 12:44:43 +0000

    cell stack

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,5 +1,7 @@
 export { default as Table } from "./Table"
 export * from "./Table"
+export { default as CellStack } from "./CellStack"
+export * from "./CellStack"
 export {
   TableCell as Cell,
   type TableCellProps as CellProps,
