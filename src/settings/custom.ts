779337e3846cf07f15148f1fba40b94c7712b8a2
SKairinos <stefan.kairinos@ocado.com>
2024-12-12 17:18:43 +0000

    fix shorthand

--- src/settings/custom.ts
+++ src/settings/custom.ts
@@ -6,18 +6,17 @@
  */
 
 // Shorthand to access environment variables.
-const env = import.meta.env
+const env = import.meta.env as Record<string, string | undefined>
 
 // The name of the current service.
-export const SERVICE_NAME: string = env.VITE_SERVICE_NAME ?? "REPLACE_ME"
+export const SERVICE_NAME = env.VITE_SERVICE_NAME ?? "REPLACE_ME"
 
 // The api url of the current service.
-export const SERVICE_API_URL: string =
+export const SERVICE_API_URL =
   env.VITE_SERVICE_API_URL ?? "http://localhost:8000"
 
 // The names of cookies.
 export const CSRF_COOKIE_NAME = `${SERVICE_NAME}_csrftoken`
-export const SESSION_COOKIE_NAME: string =
-  env.VITE_SESSION_COOKIE_NAME ?? "session_key"
-export const SESSION_METADATA_COOKIE_NAME: string =
+export const SESSION_COOKIE_NAME = env.VITE_SESSION_COOKIE_NAME ?? "session_key"
+export const SESSION_METADATA_COOKIE_NAME =
   env.VITE_SESSION_METADATA_COOKIE_NAME ?? "session_metadata"
--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,5 +1,5 @@
 // Shorthand to access environment variables.
-export default import.meta.env
+export default import.meta.env as Record<string, string | undefined>
 
 export * from "./custom"
 export * from "./vite"
