242d9442cc02e849dcaac8a4428770a48e8f1f16
SKairinos <skairinos@gmail.com>
2025-03-18 15:26:49 +0000

    fix: create vite proxy

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,5 +1,10 @@
 // Shorthand to access environment variables.
-export default import.meta.env as Record<string, string>
+export default Object.defineProperty(import.meta.env, "vite", {
+  writable: false,
+  value: new Proxy(import.meta.env, {
+    get: (target, name: string) => target[`VITE_${name}`],
+  }),
+}) as ImportMetaEnv & { vite: Record<string, string> }
 
 export * from "./custom"
 export * from "./vite"
