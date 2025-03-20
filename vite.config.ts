b76b4b8dd4b5822820577a149d13992468c1cc0c
Stefan Kairinos <skairinos@gmail.com>
2025-03-20 15:40:19 +0000

    fix: Portal frontend 48 (#82)
    * fix: create vite proxy
    
    * fix: add vite config
    
    * fix: todo
    
    * add todo
    
    * fix package.json
    
    * update lock

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
 
 // https://vitejs.dev/config/
+// TODO: https://vite.dev/guide/build.html#library-mode
+// TODO: import common configs from src/vite.config.ts
 export default defineConfig({
   plugins: [react()],
   server: {
