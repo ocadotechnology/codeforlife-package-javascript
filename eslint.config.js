3dc5246d52ac9e96e56b48f46951a68ec7cd5ffd
SKairinos <skairinos@gmail.com>
2025-08-21 09:59:29 +0000

    ignore src/scripts/*

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,12 @@
 import workspaceConfig from "@codeforlife/workspace/eslint.config.js"
 import ts from "typescript-eslint"
 
-export default ts.config(...workspaceConfig, {
-  languageOptions: { parserOptions: { tsconfigRootDir: import.meta.dirname } },
-})
+export default ts.config(
+  ...workspaceConfig,
+  { ignores: ["src/scripts/*"] },
+  {
+    languageOptions: {
+      parserOptions: { tsconfigRootDir: import.meta.dirname },
+    },
+  },
+)
