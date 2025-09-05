779854a5cf43adc09086451023ad70091b3550f9
SKairinos <skairinos@gmail.com>
2025-09-05 17:06:05 +0000

    house keeping [skip ci]

--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,6 @@ function generateEntries(...indexDirs: string[]) {
   )
 }
 
-// "./fonts/ttf/*": "./src/fonts/ttf/*.ttf",
-// "./images/svg/*": "./src/images/svg/*.svg",
-// "./server": "./src/server.js"
-
 export default defineConfig({
   plugins: [
     react(),
