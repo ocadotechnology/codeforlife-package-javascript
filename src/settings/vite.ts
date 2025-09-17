95870b966a924d066f8c8ee47fc06278aa716992
SKairinos <skairinos@gmail.com>
2025-09-17 14:54:10 +0000

    fix types

--- src/settings/vite.ts
+++ src/settings/vite.ts
@@ -8,19 +8,19 @@
 const env = import.meta.env
 
 // The mode the app is running in.
-export const MODE: string = env.MODE
+export const MODE = env.MODE
 
 // The base url the app is being served from.
 // This is determined by the base config option.
-export const BASE_URL: string = env.BASE_URL
+export const BASE_URL = env.BASE_URL
 
 // Whether the app is running in production (running the dev server with
 // NODE_ENV='production' or running an app built with NODE_ENV='production').
-export const PROD: boolean = env.PROD
+export const PROD = env.PROD
 
 // Whether the app is running in development (always the opposite of
 // import.meta.env.PROD)
-export const DEV: boolean = env.DEV
+export const DEV = env.DEV
 
 // Whether the app is running in the server.
-export const SSR: boolean = env.SSR
+export const SSR = env.SSR
