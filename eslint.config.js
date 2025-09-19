a1536e5c51766896dbcf8705c9390f0894f1ff07
SKairinos <skairinos@gmail.com>
2025-09-19 10:37:23 +0000

    house keeping

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,7 @@ import ts from "typescript-eslint"
 
 export default ts.config(
   ...workspaceConfig,
-  {
-    ignores: [
-      "src/scripts/*",
-      "src/server.js", // TODO: convert to src/server.ts and remove this ignore
-    ],
-  },
+  { ignores: ["src/scripts/*"] },
   {
     languageOptions: {
       parserOptions: { tsconfigRootDir: import.meta.dirname },
